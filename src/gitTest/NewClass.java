a4ac447d119088cd245bd87063531fd17feb079c
Green_Computer <Green_Computer@DESKTOP-912HOCH>
2020-11-10 10:29:34 +0900

    hotfix test

--- src/gitTest/NewClass.java
+++ src/gitTest/NewClass.java
@@ -5,6 +5,7 @@ public class NewClass {
 	public static void main(String[] args) {
 		System.out.println("New Class 01");
 		System.out.println("New Class 02");
-		System.out.println("Hotfix Branch");
+		System.out.println("Hotfix Branch 01");
+		System.out.println("Hotfix Branch 02");
 	}
 }
